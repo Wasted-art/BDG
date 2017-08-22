9b0c5020ced6fca50630f434e6317dec0892c852
Wasted-art <Christian@gundersen.dk>
2017-08-22 06:55:32 +0200

    header

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 		<meta name="viewport" content="width=device-width">
 
 		<link href="https://fonts.googleapis.com/css?family=Muli:300,300i,400,700,900" rel="stylesheet">
-
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<title><?php bloginfo('name'); ?></title>
 		<?php wp_head(); ?>
 	</head>
@@ -18,25 +18,17 @@
 		<header class="site-header">
 			<!--<h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
 			<h5><?php bloginfo('description'); ?></h5> -->
-			<div class="test">
-				<nav class="site-nav">
+			<nav class="site-nav">
 					
-					<?php
-					
-					$args = array(
-						'theme_location' => 'primary'
-					);
-					
-					?>
-					
-					<?php wp_nav_menu(  $args ); ?>
+				<?php
+					$args = array('theme_location' => 'primary');	
+				?>
+				<?php wp_nav_menu(  $args ); ?>
 
-				</nav>
+			</nav>
+			<div class="slidekontrast">
+				<?php masterslider(1); ?>	
 			</div>
-			<div class="slide-container">
-				<h1>yolo</h1>
-			</div>
-
 
 			
 		</header><!-- /site-header -->
--- page.php
+++ page.php
@@ -1,12 +1,11 @@
 <?php
-
 get_header();
 
 if (have_posts()) :
 	while (have_posts()) : the_post(); ?>
 	
 	<article class="post page">
-		<h2><?php the_title(); ?></h2>
+		<!-- <h2><?php the_title(); ?></h2> -->
 		<?php the_content(); ?>
 	</article>
 	
